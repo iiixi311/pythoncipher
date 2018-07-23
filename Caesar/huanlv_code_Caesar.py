acb3d74878fcb8cd86f262b13d4cc90b088dbaea
Lai Huan <laivanhuan2104@gmail.com>
2018-07-23 14:02:10 +0700

    update code

--- Caesar/huanlv_code_Caesar.py
+++ Caesar/huanlv_code_Caesar.py
@@ -1,9 +1,4 @@
-"""Encrypt the plaintext and return the ciphertext
-------key: The characters to encode
-------change: The number of shift
-------plaintext: The text to encode
-*=>encrypt return a string after encoding
-"""
+"""Encrypt the plaintext and return the ciphertext"""
 def encrypt(key,change,plaintext):
 	result = ""
 	for c in plaintext:
@@ -14,12 +9,7 @@ def encrypt(key,change,plaintext):
 			result += c
 	return result
 
-"""Decrypt the plaintext and return the plaintext
-------key: The characters to decode
-------change: The number of shift
-------plaintext: The text to encode
-*=>decrypt return a string after decoding
-"""
+"""Decrypt the plaintext and return the plaintext"""
 def decrypt(key,change,ciphertext):
 	result = ""
 	for c in ciphertext:
@@ -31,11 +21,23 @@ def decrypt(key,change,ciphertext):
 	return result
 
 """The example of caesar algorithm"""
-# key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-# text = "TOIYEUVIETNAM"
-# change = 3
-# ciphertext = encrypt(key,change,text)
-# plaintext = decrypt(key,change,ciphertext)
-# print("Text: ",text)
-# print("Ciphertext: ",ciphertext)
-# print("Plaintext: ",plaintext)
+check = True
+while check:
+	print("""Enter your chose:\n\t1. Encode\n\t2. Decode\n\t3. Exit""")
+	chose = input("Your chose is: ")
+	if chose == '1':
+		key = input("[+]Enter a custom alphabet: ")
+		text = input("[+]The text you want encode: ")
+		change = int(input("[+]Enter the shift< a number >:"))
+		ciphertext = encrypt(key,change,text)
+		print("*The text after encode: ",ciphertext+"\n"+"*"*35)
+	elif chose == '2':
+		key = input("[+]Enter a custom alphabet: ")
+		text = input("[+]The text you want decode: ")
+		change = int(input("[+]Enter the shift< a number >:"))
+		plaintext = decrypt(key,change,text)
+		print("*The text after decode: ",plaintext+"\n"+"*"*35)
+	elif chose == '3':
+		check = False
+	else:
+		print("ERROR! You must chose 1,2 or 3.\n"+"*"*35)
